4accccc3bf1fa74cf1daa4d301d5070bc7dd0008
Sogand <sogandmohamadipour7@gmail.com>
2026-02-07 21:27:01 +0330

    using render as u fetch strategy

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+
 function Menu() {
+  useLoaderData();
   return <h1>Menu</h1>;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
