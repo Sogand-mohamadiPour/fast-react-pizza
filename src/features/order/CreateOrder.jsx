dcecf66a1d120ee756d49eda7e4d5043094e7142
Sogand <sogandmohamadipour7@gmail.com>
2026-02-09 21:54:08 +0330

    error handling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect, useNavigation, } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -35,6 +35,8 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
 
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
@@ -96,9 +98,16 @@ export async function action({ request }) {
     priority: data.priority === "on",
   };
 
-  const newOrder = await createOrder(order);
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone =
+      "Please give us your correct phone number. we might need it to cantact you";
+  }
 
-  
+  if (Object.keys(errors).length > 0) return errors;
+
+  // if everything is ok create new order and redirect
+  const newOrder = await createOrder(order);
 
   return redirect(`/order/${newOrder.id}`);
 }
