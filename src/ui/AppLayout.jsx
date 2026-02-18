59e25718350630d01d911132249272b80aab5a60
Sogand <sogandmohamadipour7@gmail.com>
2026-02-18 14:55:56 +0330

    grid with tailwind

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,6 @@
-import { Outlet, useNavigation } from "react-router-dom"
-import CartOverview from "../features/cart/CartOverview"
-import Header from "./Header"
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
 import Loader from "./Loader";
 
 function AppLayout() {
@@ -8,18 +8,20 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
-      {isLoading && <Loader /> }
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
 
       <Header />
 
-      <main>
-        <Outlet />
-      </main>
+      <div> 
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
-  )
+  );
 }
 
-export default AppLayout
+export default AppLayout;
