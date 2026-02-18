f87aaf409a1164cf9d39a6a3a0164623e6f3ce26
Sogand <sogandmohamadipour7@gmail.com>
2026-02-18 15:14:06 +0330

    styiling button and some elements and states in tailwind

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -80,7 +80,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="inline-block cursor-pointer rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed"
+          >
             {isSubmitting ? "placing order...." : "order now"}
           </button>
         </div>
