d15dd1d357c647e6b4924a5266fda642bc3bc71e
Sogand <sogandmohamadipour7@gmail.com>
2026-02-18 13:08:46 +0330

    spacing with tailwind

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Sogand</div>;
+  return <div className="hidden text-sm font-semibold">Sogand</div>;
 }
 
 export default Username;
